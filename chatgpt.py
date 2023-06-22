cb172f06626d1823d0358ecab9cd346894b8f0f6
Mike Stromme <mstromme@suburbanenterprises.com>
2023-06-22 15:33:41 -0500

    changes

--- chatgpt.py
+++ chatgpt.py
@@ -1,3 +1,20 @@
 import openai
 
 
+openai.api_key = "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"
+
+def BasicGeneration(userPrompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": userPrompt}]
+    )
+    return completion.choices[0].message.content
+
+
+userPrompt = "What does it mean to be an optimist?"
+
+response = BasicGeneration(userPrompt)
+
+print(response)
+
